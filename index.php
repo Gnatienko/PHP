8d6a330db19832f0f4a06106a6cea89ab612bd5b
HP <gnatienko@gmail.com>
2021-03-30 15:33:54 -0400

    test2

--- index.php
+++ index.php
@@ -5,9 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <title>Reader</title>
-
-
-
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
@@ -15,23 +12,12 @@
  </head>
  <body>
 
-
-
-
    <div>
-
-
-
-
-
-
      <form action="/text_upload.php" method="post">
-       <textarea  type="text" name="contentForm" id="contentForm" placeholder="Please insert text..." class="form-control"></textarea>
+       <textarea  name="contentForm" id="contentForm" placeholder="Please insert text" class="form-control"></textarea>
        <div class="btn-align">
          <button type="submit" name="Upload_text" class="btn btn-success">Upload</button>
        </div>
-
-
 
      </form>
 
